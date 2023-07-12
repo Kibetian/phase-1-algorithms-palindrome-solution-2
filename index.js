330c4aa91eb38595309f6ecc9ae2ed5bda3293fe
ian kibet <iankibet87@gmail.com>
2023-07-12 10:57:35 +0800

    palindrome soltn

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  for(let i = 0; i < word.length / 2; i++) {
+    const j = word.length -1 -i
+    const start = word[i];
+    const end = word[j];
+
+    if(start !== end)
+    return false
+
+  }
+  return true;
 }
 
 /* 
   Add your pseudocode here
+*/
+  /* 
+ iterate from the beginning of the string to the middle of the string
+  compare the letter we're iterating over to the corresponding letter at the end of the string
+    if the letters don't match, return false
+
+if we reach the middle, and all the letters match, return true
 */
 
 /*
@@ -20,6 +37,9 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
 }
 
 module.exports = isPalindrome;
